679317acae27aa368abda7f6ce4625ed7e1e029b
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-08 14:20:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,7 @@ holiday_hash.each do |key, value|
   puts "#{key.capitalize}:"
   value.each do |holiday, supply| 
     holiday = holiday.to_s.split("_").each { |word| word.capitalize}.join{' '}
-    supply = supply.to_s.split(",")
-    supply = supply.capitalize!.join (" ,")
+    supply = supply.to_s.join(", ")
     puts "#{holiday}: #{supply}"
   end
 
