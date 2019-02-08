8c4d5894cbcc2d99624150893060f2ea7379cc9a
sensible-shell-8529 <diana.lee116@gmail.com>
2019-02-08 14:52:40 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,6 +70,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+array = []
 holiday_hash.each do |key, value| 
   value.each do | key1, value1| 
     if value1.include?("BBQ")
